5c899087758c7ebcad3347e830c7584e26bfcc8c
Mohammad Assad <assad62@hotmail.com>
2023-11-20 12:12:06 -0500

    added web support

--- lib/home/home_bloc.dart
+++ lib/home/home_bloc.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:glad/music_service/music_service.dart';
 import 'package:rxdart/src/streams/value_stream.dart';
--- lib/music_service/music_service.dart
+++ lib/music_service/music_service.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+
+//import 'package:assets_audio_player_web/assets_audio_player_web.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:rxdart/src/streams/value_stream.dart';
 
@@ -7,7 +9,19 @@ class MusicService{
   late AssetsAudioPlayer _assetsAudioPlayer;
   final List<StreamSubscription> _subscriptions = [];
   late final _audios = <Audio>[
-    Audio("assets/a.mp3"),
+    Audio.network(
+      'https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Music_for_Video/springtide/Sounds_strange_weird_but_unmistakably_romantic_Vol1/springtide_-_03_-_We_Are_Heading_to_the_East.mp3',
+      metas: Metas(
+        id: 'Online',
+        title: 'Online',
+        artist: 'Florent Champigny',
+        album: 'OnlineAlbum',
+        // image: MetasImage.network('https://www.google.com')
+        image: MetasImage.network(
+            'https://image.shutterstock.com/image-vector/pop-music-text-art-colorful-600w-515538502.jpg'),
+      ),
+    ),
+    Audio("assets/a.mp3",metas: Metas()),
     Audio("assets/b.mp3"),
     Audio("assets/c.mp3")
   ];
@@ -35,6 +49,7 @@ class MusicService{
   }
 
   void openPlayer() async{
+
     await _assetsAudioPlayer.open(
       Playlist(audios: _audios, startIndex: 0),
       showNotification: false,
